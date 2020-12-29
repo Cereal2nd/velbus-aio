70df2077f9d281474c1938ea67e7e48920085586
cereal2nd <maikel.punie@gmail.com>
2020-12-29 11:03:28 +0100

    New version

--- velbusaio/handler.py
+++ velbusaio/handler.py
@@ -1,18 +1,17 @@
-import asyncio
 import logging
 import itertools
 import json
 import re
 from velbusaio.message import Message
-from velbusaio.module import Module
 from velbusaio.helpers import keys_exists, h2
 from velbusaio.const import *
 
 
 class PacketHandler:
-    def __init__(self, protocol):
+    def __init__(self, writer, velbus):
         self._log = logging.getLogger("velbus-packet")
-        self.protocol = protocol
+        self._writer = writer
+        self._velbus = velbus
         self.modules = dict()
         with open(
             "/home/cereal/velbus-aio/velbusaio/moduleprotocol/protocol.json"
@@ -29,63 +28,23 @@ class PacketHandler:
             self._log.debug("scan")
         elif msg.msgtype == 0xFF:
             await self._handleModuleType(msg)
-        del msg
-        return
-        if msg.address not in self.modules:
+        # from this point onwards we should know the module
+        # if not (yet) then ddo not handle the packet
+        if msg.address not in self._velbus.get_modules():
             self._log.warning(
                 "Module not found (yet): {} !!!!!!!!!!!".format(msg.address)
             )
             return
+        # handle the global packets
         if msg.msgtype == 0xB0:
-            self._handleModulesubType(msg)
+            await self._handleModuleSubType(msg)
         elif msg.msgtype == 0xD8:
             self._log.debug("Realtime clock status => ignore")
         elif msg.msgtype == 0xB7:
             self._log.debug("Date sync => ignore")
-        elif msg.msgtype == 0xE6:
-            self._handleTemperature(msg)
-        elif msg.msgtype in [0xF0, 0xF1, 0xF2]:
-            self._handleChannelName(msg)
-        elif msg.msgtype in [0xCC, 0xFE]:
-            self._log.debug("Memory data")
-        else:
-            self._handleModulespecifick(msg)
+        # else:
+        #    self._log.debug("HAND OFFF")
         del msg
-
-    def _handleModulespecifick(self, msg):
-        cmsg = keys_exists(
-            self.pdata,
-            "ModuleTypes",
-            h2(self.modules[msg.address]["type"]),
-            "Messages",
-            h2(msg.msgtype),
-        )
-        if not cmsg:
-            self._log.warning(
-                "IGNORE MESSAGE: messagetype not found for module {} {}".format(
-                    h2(self.modules[msg.address]["type"]), h2(msg.msgtype)
-                )
-            )
-            return
-        self._log.debug("{0} received".format(cmsg["Info"]))
-        result = None
-        if "Data" in cmsg and "PerByte" in cmsg["Data"]:
-            result = self._perByte(cmsg["Data"]["PerByte"], msg)
-        elif "Data" in cmsg and "PerMessage" in cmsg["Data"]:
-            result = self._perMessage(cmsg["Data"]["PerMessage"], msg)
-        if not result:
-            self._log.warning("IGNORE MESSAGE: no parsing info {}".format(cmsg))
-            return
-        if "Channel" in result and "Value" in result:
-            self.modules[msg.address]["channels"][result["Channel"]]["Value"] = result[
-                "Value"
-            ]
-        else:
-            self._log.debug("Result: {}".format(result))
-
-    def _perMessage(self, cmsg, msg):
-        print("Permessage")
-        print(cmsg)
 
     def _perByte(self, cmsg, msg):
         result = dict()
@@ -136,11 +95,11 @@ class PacketHandler:
         self._log.info("LOADING MODULE {}".format(addr))
         # request the module status (if available for this module
         if keys_exists(self.pdata, "ModuleTypes", h2(mtype), "Messages", "FA"):
-            for chan in list(self.modules[addr]["channels"]):
+            for chan in self._velbus.get_channels(addr):
                 msg = Message()
                 msg.address = addr
                 msg.data = bytearray([0xFA, int(chan)])
-                await self.protocol.send(msg)
+                await self._writer(msg)
         # request the module channel names
         d = keys_exists(self.pdata, "ModuleTypes", h2(mtype), "AllChannelStatus")
         if d:
@@ -148,13 +107,13 @@ class PacketHandler:
             msg = Message()
             msg.address = addr
             msg.data = bytearray([0xEF, 0xFF])
-            await self.protocol.send(msg)
+            await self._writer(msg)
         else:
             for chan in list(self.modules[addr]["channels"]):
                 msg = Message()
                 msg.address = addr
                 msg.data = bytearray([0xEF, int(chan)])
-                await self.protocol.send(msg)
+                await self._writer(msg)
 
     async def _handleModuleType(self, msg):
         self._log.debug("Module type: answer to a Scan")
@@ -164,9 +123,20 @@ class PacketHandler:
             self._log.warning("Module not recognized: {}".format(msg.data[0]))
             return
         # create the module
-        self.modules[msg.address] = Module(msg.address, msg.data[0], d, self.protocol)
-        # load all params for the module
-        await self.modules[msg.address].load()
+        await self._velbus.add_module(msg.address, msg.data[0], d)
+
+    async def _handleModuleSubType(self, msg):
+        self._log.debug("Module subtype: answer to a Scan")
+        if msg.address not in self.modules:
+            return
+        # remove moduletype, high serial, low serial
+        del msg.data[0:3]
+        # store all subaddresses
+        # and link back to the main module
+        for num, subAddr in enumerate(msg.data):
+            if subAddr == 0xFF:
+                continue
+            await self._velbus.add_module(msg.address, msg.data[0], None, subAddr, num)
 
     def _channelConvert(self, module, channel, ctype):
         d = keys_exists(self.pdata, "ModuleTypes", h2(module), "ChannelNumbers", ctype)
@@ -179,57 +149,3 @@ class PacketHandler:
                 if channel & (1 << offset):
                     return offset + 1
             return None
-
-    def _handleModuleSubType(self, msg):
-        self._log.debug("Module subtype: answer to a Scan")
-        if msg.address not in self.modules:
-            return
-        self.modules[msg.address]["subAddr"] = dict()
-        mtype = msg.data[0]
-        # remove moduletype, high serial, low serial
-        del msg.data[0:3]
-        # store all subaddresses
-        for num, subAddr in enumerate(msg.data):
-            if subAddr == 0xFF:
-                continue
-            self.modules[msg.address]["subAddr"][num] = subAddr
-            self.modules[subAddr] = {"master": msg.address, "offset": (num * 8) + 1}
-
-        # store thermostat
-        d = keys_exists(self.pdata, "ModuleTypes", h2(mtype), "ThermostatAddr")
-        if d and msg.data[int(d)] is not 0xFF:
-            self.modules[msg.address]["thermostatAddr"] = msg.data[int(d)]
-
-    def _handleChannelName(self, msg):
-        channel = self._channelConvert(
-            self.modules[msg.address]["type"], msg.data.pop(0), "Name"
-        )
-        self._log.debug(
-            "Channel name part {} ({},{}) = {}".format(
-                self.modules[msg.address]["typeName"],
-                msg.address,
-                channel,
-                msg.__dict__,
-            )
-        )
-        # clean out the channel name
-        if msg.msgtype == 0xF0:
-            self.modules[msg.address]["channels"]["{:0>2}".format(channel)]["Name"] = ""
-        # store the chars
-        for cha in msg.data:
-            if cha == 0xFF:
-                break
-            self.modules[msg.address]["channels"]["{:0>2}".format(channel)][
-                "Name"
-            ] += chr(cha)
-
-    def _handleTemperature(self, msg):
-        # find the chan with "'Type': 'Temperature'" and store it
-        temperature = (((msg.data[0] << 8) | msg.data[1]) / 32) * 0.0625
-        for i, y in self.modules[msg.address]["channels"].items():
-            if y["Type"] == "Temperature":
-                self.modules[msg.address]["channels"][i]["Value"] = temperature
-        self._log.debug("Temperature {0} for {1}".format(temperature, msg.address))
-
-    def signal_channel_update(self, address, channel, valueType, value):
-        print("UPDATE: {0} {1} {2} {3}".format(address, channel, valueType, value))
