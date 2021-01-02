b5e698ce1b979407ff84c0192e6344d34960d7a4
cereal2nd <maikel.punie@gmail.com>
2021-01-02 09:58:56 +0100

    Add an example, controller.py pylint fix

---
+++ examples/scan.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import asyncio
+import logging
+import sys
+from velbusaio.controller import Velbus
+
+
+async def main():
+    velbus = Velbus("192.168.1.9", 27015, True)
+    await velbus.connect()
+    for mod in (velbus.get_modules()).values():
+        print(mod.__dict__)
+        print("")
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.getLogger("asyncio").setLevel(logging.DEBUG)
+asyncio.run(main(), debug=False)
