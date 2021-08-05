d90d4f44949f9c716923e1c42d7775eb7925a4ff
Maikel Punie <maikel.punie@gmail.com>
2021-08-05 09:11:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="velbus-aio",
-    version="2021.8.1",
+    version="2021.8.2",
     url="https://github.com/Cereal2nd/velbus-aio",
     license="MIT",
     author="Maikel Punie",
