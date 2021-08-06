ee029152986dfd936dcd5ff464a0d242954b5073
cereal2nd <maikel.punie@gmail.com>
2021-08-06 14:17:01 +0200

    Removed asyncio as its already included

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url="https://github.com/Cereal2nd/velbus-aio",
     license="MIT",
     author="Maikel Punie",
-    install_requires=["asyncio", "pyserial-asyncio"],
+    install_requires=["pyserial-asyncio"],
     author_email="maikel.punie@gmail.com",
     packages=["velbusaio", "velbusaio.messages"],
     platforms="any",
