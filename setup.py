49f25ae63d1aa68033a1bc141f0bff652a1254e8
cereal2nd <maikel.punie@gmail.com>
2021-08-09 21:03:26 +0200

    fix package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="velbus-aio",
-    version="2021.8.5",
+    version="2021.8.6",
     url="https://github.com/Cereal2nd/velbus-aio",
     license="MIT",
     author="Maikel Punie",
