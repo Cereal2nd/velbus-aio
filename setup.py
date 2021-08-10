48b57ccf7e537a97b4076063bf3435e396a390e3
cereal2nd <maikel.punie@gmail.com>
2021-08-10 16:49:28 +0200

    Try to fix the wheel package

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name="velbus-aio",
-    version="2021.8.6",
+    version="2021.8.7",
     url="https://github.com/Cereal2nd/velbus-aio",
     license="MIT",
     author="Maikel Punie",
     install_requires=["pyserial-asyncio"],
     author_email="maikel.punie@gmail.com",
     packages=["velbusaio", "velbusaio.messages"],
+    include_package_data=True,
     platforms="any",
 )
