951648fd064e9de8c7f7a826111adab13249c726
Maikel Punie <maikel.punie@gmail.com>
2021-10-21 16:20:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PACKAGES = find_packages(exclude=["tests", "tests.*"])
 
 setup(
     name="velbus-aio",
-    version="2021.10.5",
+    version="2021.10.6",
     url="https://github.com/Cereal2nd/velbus-aio",
     license="MIT",
     author="Maikel Punie",
