901bb779a67f076a852c77e8aa2744de6ba2ffb8
niobos <niobos@dest-unreach.be>
2022-10-07 06:43:56 +0200

    Move connect string to command line argument (#45)
    Co-authored-by: Niels Laukens <niels@dest-unreach.be>

--- examples/load_modules.py
+++ examples/load_modules.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
+import argparse
 import asyncio
 import logging
 import sys
 
 from velbusaio.controller import Velbus
 
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--connect", help="Connection string", default="192.168.1.254:8445")
+args = parser.parse_args()
 
-async def main():
+
+async def main(connect_str: str):
     # SET THE connection params below
     # example via signum:
     #   velbus = Velbus("tls://192.168.1.9:27015")
@@ -15,7 +20,7 @@ async def main():
     #   velbus = Velbus("192.168.1.9:27015")
     # example via serial device
     #   velbus = Velbus("/dev/ttyAMA0")
-    velbus = Velbus("192.168.1.254:8445")
+    velbus = Velbus(connect_str)
     await velbus.connect()
     for mod in (velbus.get_modules()).values():
         print(mod)
@@ -25,4 +30,4 @@ async def main():
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger("asyncio").setLevel(logging.DEBUG)
-asyncio.run(main(), debug=False)
+asyncio.run(main(args.connect), debug=False)
