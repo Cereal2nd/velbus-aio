baa042748f6794125afec28d276348ed00e66d43
Maikel Punie <maikel.punie@gmail.com>
2022-11-05 18:12:46 +0100

    Prepare for the next release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ PACKAGES = find_packages(exclude=["tests", "tests.*"])
 
 setup(
     name="velbus-aio",
-    version="2022.10.4",
+    version="2022.11.0",
     url="https://github.com/Cereal2nd/velbus-aio",
     license="MIT",
     author="Maikel Punie",
